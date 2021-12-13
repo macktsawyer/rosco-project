5dfeca66c2208c8a00861c8f75ccf30f5c061900
macktsawyer <macktsawyer@yahoo.com>
2021-12-12 23:36:37 -0500

    Button style changes

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -2,21 +2,8 @@ import React from 'react';
 import { StyledContainer } from '../Styles/Container.styled.js';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import styled from 'styled-components';
+import { StyledButton } from '../Styles/Button.styled.js';
 
-const Button = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    border: 1px solid black;
-    border-radius: 5px;
-    background: #3578D9;
-    color: white;
-    width: 100px;
-    height: 50px;
-    h1 {
-        transform: translate(0, -225%);
-    }
-`;
 
 const Buttons = () => {
     return (
@@ -24,7 +11,7 @@ const Buttons = () => {
             <h3>Basic Buttons</h3>
             <StyledContainer className="center">
                 <StyledContainer>
-                    <Button><h1>Hello</h1></Button>
+                    <StyledButton><h1>Hello</h1></StyledButton>
                 </StyledContainer>
             </StyledContainer>
         </div>
---
+++ src/Styles/Button.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.div`
+display: flex;
+justify-content: center;
+align-content: center;
+border: 1px solid black;
+border-radius: 5px;
+background: #3578D9;
+color: white;
+width: 100px;
+height: 50px;
+cursor: pointer;
+    &:hover {
+        background-color: #4288ed;
+    }
+    h1 {
+        transform: translate(0, -225%);
+    }
+`;
